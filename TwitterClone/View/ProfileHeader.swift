9e00313b6f11bf4bee6dfc4cc0f6241ac1d84022
Jcole429 <Justincole29@hotmail.com>
2021-09-20 14:55:50 -0400

    Using delegate methods to animate filter UI

--- TwitterClone/View/ProfileHeader.swift
+++ TwitterClone/View/ProfileHeader.swift
@@ -80,10 +80,18 @@ class ProfileHeader: UICollectionReusableView {
         return label
     }()
     
+    private let underLineView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .twitterBlue
+        return view
+    }()
+    
     // MARK: - Lifecycle
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        
+        filterBar.delegate = self
         
         addSubview(containerView)
         containerView.anchor(top: topAnchor, left: leftAnchor, right: rightAnchor, height: 108)
@@ -105,6 +113,9 @@ class ProfileHeader: UICollectionReusableView {
         addSubview(filterBar)
         filterBar.anchor(left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, height: 50)
         
+        addSubview(underLineView)
+        underLineView.anchor(left: leftAnchor, bottom: bottomAnchor, width: frame.width / 3, height: 2)
+        
     }
     
     required init?(coder: NSCoder) {
@@ -119,5 +130,17 @@ class ProfileHeader: UICollectionReusableView {
     
     @objc func handleEditProfileFollow() {
         
+    }
+}
+
+// MARK: - ProfileFilterViewDelegate
+
+extension ProfileHeader: ProfileFilterViewDelegate {
+    func filterView(_ view: ProfileFilterView, didSelect indexPath: IndexPath) {
+        guard let cell = view.collectionView.cellForItem(at: indexPath) as? ProfileFilterCell else {return}
+        let xPosition = cell.frame.origin.x
+        UIView.animate(withDuration: 0.3) {
+            self.underLineView.frame.origin.x = xPosition
+        }
     }
 }
