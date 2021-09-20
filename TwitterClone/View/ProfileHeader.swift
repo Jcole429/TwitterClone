c3331ac6b8659eac412d8cce8e2b1f1e70444972
Jcole429 <Justincole29@hotmail.com>
2021-09-20 13:21:54 -0400

    Setting up initial profile CollectionViewController and header

---
+++ TwitterClone/View/ProfileHeader.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileHeader.swift
+//  TwitterClone
+//
+//  Created by Justin Cole on 9/20/21.
+//
+
+import UIKit
+
+class ProfileHeader: UICollectionReusableView {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .red
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
