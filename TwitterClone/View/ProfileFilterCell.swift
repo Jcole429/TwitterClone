9923dae0b35a5fda836244073d0f8201dc8539cf
Jcole429 <Justincole29@hotmail.com>
2021-09-20 14:26:28 -0400

    Adding the profile filter UI

--- TwitterClone/View/ProfileFilterCell.swift
+++ TwitterClone/View/ProfileFilterCell.swift
@@ -5,4 +5,38 @@
 //  Created by Justin Cole on 9/20/21.
 //
 
-import Foundation
+import UIKit
+
+class ProfileFilterCell: UICollectionViewCell {
+    
+    // MARK: - Properties
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .lightGray
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.text = "Test filter"
+        return label
+    }()
+    
+    override var isSelected: Bool {
+        didSet {
+            titleLabel.font = isSelected ? UIFont.boldSystemFont(ofSize: 16) : UIFont.boldSystemFont(ofSize: 14)
+            titleLabel.textColor = isSelected ? .twitterBlue : .lightGray
+        }
+    }
+    
+    // MARK: - Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .white
+        
+        addSubview(titleLabel)
+        titleLabel.center(inView: self)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
