535a143e0768844bbe398bb5cf3fbd4c64c893fc
Jcole429 <Justincole29@hotmail.com>
2021-09-21 19:29:32 -0400

    Start of tweet display controller

---
+++ TwitterClone/View/TweetHeader.swift
@@ -0,0 +1,24 @@
+//
+//  TweetHeader.swift
+//  TwitterClone
+//
+//  Created by Justin Cole on 9/21/21.
+//
+
+import UIKit
+
+class TweetHeader: UICollectionReusableView {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .systemPink
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
