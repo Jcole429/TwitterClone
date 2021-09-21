e9c99b55f790bdc24b68d4368807547905bedb1d
Jcole429 <Justincole29@hotmail.com>
2021-09-21 11:53:38 -0400

    Implementing searching in explore view

--- TwitterClone/Controller/ExploreController.swift
+++ TwitterClone/Controller/ExploreController.swift
@@ -19,12 +19,25 @@ class ExploreController: UITableViewController {
         }
     }
     
+    private var filteredUsers = [User]() {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    
+    private var inSearchMode: Bool {
+        return searchController.isActive && !searchController.searchBar.text!.isEmpty
+    }
+    
+    private let searchController = UISearchController(searchResultsController: nil)
+    
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
         fetchUsers()
+        configureSearchController()
     }
     
     // MARK: - API
@@ -45,16 +58,35 @@ class ExploreController: UITableViewController {
         tableView.rowHeight = 60
         tableView.separatorStyle = .none
     }
+    
+    func configureSearchController() {
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.hidesNavigationBarDuringPresentation = false
+        searchController.searchBar.placeholder = "Search for a user"
+        navigationItem.searchController = searchController
+        definesPresentationContext = false
+    }
 }
 
 extension ExploreController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return users.count
+        return inSearchMode ? filteredUsers.count : users.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! UserCell
-        cell.user = users[indexPath.row]
+        let user = inSearchMode ? filteredUsers[indexPath.row] : users[indexPath.row]
+        cell.user = user
         return cell
+    }
+}
+
+extension ExploreController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let searchText = searchController.searchBar.text?.lowercased() else {return}
+        filteredUsers = users.filter({ user in
+            return user.username.lowercased().contains(searchText) || user.fullname.lowercased().contains(searchText)
+        })
     }
 }
