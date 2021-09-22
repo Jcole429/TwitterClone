a6018867c97b2e6c442dc283d752210f8228e800
Jcole429 <Justincole29@hotmail.com>
2021-09-21 22:02:41 -0400

    Removing debug text

--- TwitterClone/View/TweetCell.swift
+++ TwitterClone/View/TweetCell.swift
@@ -90,7 +90,6 @@ class TweetCell: UICollectionViewCell {
         stack.anchor(top: profileImageView.topAnchor, left: profileImageView.rightAnchor, right: rightAnchor, paddingLeft: 12, paddingRight: 12)
         
         infoLabel.font = UIFont.systemFont(ofSize: 14)
-        infoLabel.text = "Eddie Brock @venom"
         
         let actionStack = UIStackView(arrangedSubviews: [commentButton, retweetButton, likeButton, shareButton])
         actionStack.axis = .horizontal
