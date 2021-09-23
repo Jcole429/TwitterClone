4269df4a5be4cc206d3e094d245f7214949bd2be
Jcole429 <Justincole29@hotmail.com>
2021-09-23 15:26:30 -0400

    Displaying and dismissing ActionSheet

--- TwitterClone/Utils/ActionSheetLauncher.swift
+++ TwitterClone/Utils/ActionSheetLauncher.swift
@@ -17,6 +17,17 @@ class ActionSheetLauncher: NSObject {
     private let tableView = UITableView()
     private var window: UIWindow?
     
+    private lazy var blackView: UIView = {
+        let view = UIView()
+        view.alpha = 0
+        view.backgroundColor = UIColor(white: 0, alpha: 0.5)
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleDismissal))
+        view.addGestureRecognizer(tap)
+        
+        return view
+    }()
+    
     // MARK: - Lifecycle
     
     init(user: User) {
@@ -26,16 +37,31 @@ class ActionSheetLauncher: NSObject {
         configureTableView()
     }
     
+    // MARK: - Selectors
+    
+    @objc func handleDismissal() {
+        UIView.animate(withDuration: 0.5) {
+            self.blackView.alpha = 0
+            self.tableView.frame.origin.y += 300
+        }
+    }
+    
     // MARK: - Helpers
     
     func show() {
-        print("DEBUG: Show actionsheet for user \(user.username)")
-        
         guard let window = UIApplication.shared.windows.first(where: { $0.isKeyWindow }) else {return}
         self.window = window
         
+        window.addSubview(blackView)
+        blackView.frame = window.frame
+        
         window.addSubview(tableView)
-        tableView.frame = CGRect(x: 0, y: window.frame.height - 300, width: window.frame.width, height: 300)
+        tableView.frame = CGRect(x: 0, y: window.frame.height, width: window.frame.width, height: 300)
+        
+        UIView.animate(withDuration: 0.5) {
+            self.blackView.alpha = 1
+            self.tableView.frame.origin.y -= 300
+        }
     }
     
     func configureTableView() {
@@ -52,6 +78,7 @@ class ActionSheetLauncher: NSObject {
 }
 
 extension ActionSheetLauncher: UITableViewDataSource {
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 3
     }
@@ -60,8 +87,6 @@ extension ActionSheetLauncher: UITableViewDataSource {
         let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
         return cell
     }
-    
-    
 }
 
 extension ActionSheetLauncher: UITableViewDelegate {
