3cd42f97a45bfac5fd09ca161f9ffdd94d7032d0
Jcole429 <Justincole29@hotmail.com>
2021-09-25 10:00:46 -0400

    Setting up notification UI foundation

---
+++ TwitterClone/View/NotificationCell.swift
@@ -0,0 +1,65 @@
+//
+//  NotificationCell.swift
+//  TwitterClone
+//
+//  Created by Justin Cole on 9/25/21.
+//
+
+import UIKit
+
+class NotificationCell: UITableViewCell {
+    
+    // MARK: - UI Constants
+    static private let profileImageSideLength: CGFloat = 40
+    
+    // MARK: - Properties
+    
+    private lazy var profileImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        iv.setDimensions(width: NotificationCell.profileImageSideLength, height: NotificationCell.profileImageSideLength)
+        iv.makeCircle(sideLength: NotificationCell.profileImageSideLength)
+        iv.backgroundColor = .twitterBlue
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleProfileImageTapped))
+        
+        iv.addGestureRecognizer(tap)
+        iv.isUserInteractionEnabled = true
+        
+        return iv
+    }()
+    
+    private let notificationLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 2
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.text = "Some test notification message"
+        return label
+    }()
+    
+    // MARK: - Lifecycle
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        let stack = UIStackView(arrangedSubviews: [profileImageView, notificationLabel])
+        stack.spacing = 8
+        stack.axis = .horizontal
+        stack.alignment = .center
+        
+        addSubview(stack)
+        stack.centerY(inView: self, leftAnchor:  leftAnchor, paddingLeft: 12)
+        stack.anchor(right: rightAnchor, paddingRight: 12)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Selectors
+    
+    @objc func handleProfileImageTapped() {
+        
+    }
+}
